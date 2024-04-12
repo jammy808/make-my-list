8a81054a91089eb8c2b1a360a316ee65360f502e
soham <jambhwadekarsoham@gmail.com>
2024-04-12 21:21:26 +0530

    add and delete feauters of todo with database

--- src/app/api/users/add/route.ts
+++ src/app/api/users/add/route.ts
@@ -5,15 +5,21 @@ import {NextRequest,NextResponse} from 'next/server'
 import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { getDataFromToken } from '@/helpers/getDatafromToken'
-import { cookies } from 'next/headers'
 
 connect()
 
-export async function GET(request : NextRequest){
-    const userId = '6618205486157b02f55a53ab'
-     
-    const user = await User.findOne({_id: userId}).select("-password")
+export async function POST(request : NextRequest){
 
+    const reqBody = await request.json()
+        const {task} = reqBody
+        console.log(task);
+
+    const userId = await getDataFromToken(request)
+     
+    const user = await User.findOne({_id: userId.id})
+    user.tasks.push(task)
+    await user.save()
+ 
     return NextResponse.json({
         message: "User found",
         data: user
--- src/helpers/getDatafromToken.ts
+++ src/helpers/getDatafromToken.ts
@@ -1,9 +1,10 @@
 import { NextRequest } from "next/server";
+import { cookies } from 'next/headers'
 import jwt from 'jsonwebtoken'
 
-export const getDataFromToken = (request: NextRequest) =>{
+export const getDataFromToken = async (request: NextRequest) =>{
     try {
-        const token = request.cookies.get("token")?.value || "";
+        const token = await request.cookies.get("token")?.value || "";
         const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!)
 
         return decodedToken
